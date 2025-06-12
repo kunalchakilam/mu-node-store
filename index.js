b7640bf95682651ac24c16a529680b700c363f08
kunalchakilam <ikunalchakilam@gmail.com>
2025-06-12 23:22:30 +0530

    updated

--- index.js
+++ index.js
@@ -4,20 +4,32 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 app.listen(8080, () => {
   console.log("Server started");
 });
 
 let products = [];
+let users = []; 
 
 app.post("/products", async (req, res) => {
   console.log(req.body);
   const body = await req.body;
   products.push(body);
-  res.json(body)
+  res.json(body);
 });
 
 app.get("/products", async (req, res) => {
   res.json(products);
+});
 
+app.post("/users", async (req, res) => {
+  console.log(req.body);
+  const body = await req.body;
+  users.push(body);
+  res.json(body);
+});
+
+app.get("/users", async (req, res) => {
+  res.json(users);
 });
